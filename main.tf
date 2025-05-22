53dbee1993c1db347847c0da07d798d076117d27
Ankit Kumar Sinha <72785112+ankitrafay@users.noreply.github.com>
2025-05-22 17:16:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Define variables
+
+variable "host_cluster_name" {
+  description = "The name of the host cluster"
+  type        = string
+  validation {
+    condition     = length(var.host_cluster_name) > 0
+    error_message = "The cluster name must not be empty."
+  }
+}
+
+variable "namespace_quota_size" {
+  type    = string
+  default = "small"
+}
+
+
+variable "device_details" {
+  description = "Device/node on which vcluster pods will be deployed"
+  type        = any
+  default     = {}
+}
+
+output "host_cluster_name" {
+  value = var.host_cluster_name
+}
+
+output "device_details" {
+  description = "Raw device details JSON blob"
+  value       = var.device_details
+}
